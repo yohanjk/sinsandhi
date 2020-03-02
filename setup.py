6906fa4dc8bdcf92543f034747ac32e79fc204a6
yohanjk <yohan.13@cse.mrt.ac.lk>
2020-03-02 18:47:03 +0530

    updating setup.py to work with submodule when packaged

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sinsandhi',
     version='',
-    packages=['sinsandhi'],
+    packages=find_packages(),
     url='https://github.com/yohanjk/sinsandhi',
     license='',
     author='Yohan Karunanayake',
